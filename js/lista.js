8067615606950971204eec412c1c545fe5995faa
Odinlowe1651 <odinlowe1651@gmail.com>
2025-04-05 01:23:20 +0000

    Cambio1

---
+++ js/lista.js
@@ -0,0 +1,28 @@
+function mostrarLista(listaPokes){
+    const app = document.getElementById("app");
+    
+    let Pokes = " ";
+
+    const seccion = document.createElement("section");
+    seccion.classList.add("c-lista"); 
+
+/*
+    const buscador = document.createElement("input");
+    buscador.classList.add("c-buscador");
+    buscador.type = "text"
+    buscador.addEventListener("input", buscarPoke);
+*/
+    for (let i = 0; i < listaPokes.length; i++) {
+        let id = listaPokes[i].url.split("/")[6];
+        Pokes += `
+        <div class="c-lista-pokemon poke-${id}" onclick="mostrarDetalle('${listaPokes[i].name}')">
+            <p>#${id}</p>
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png" width="auto" height="60" loading="lazy" alt="${listaPokes[i].name}">
+            <p>${listaPokes[i].name}</p>
+        </div>`;
+    }
+    seccion.innerHTML = Pokes;
+    
+    /*app.appendChild(buscador);*/
+    app.appendChild(seccion);
+}
